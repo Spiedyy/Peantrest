05fa881a517227b425d61782b032e0c079c51a9c
Damian <damianeijlders@gmail.com>
2024-10-18 19:26:28 +0200

    Recoverd files

--- pintrest/lib/response.ts
+++ pintrest/lib/response.ts
@@ -1,5 +1,3 @@
-import { SessionOptions } from "iron-session";
-
 export interface Image {
     id: number;
     img: string;
@@ -14,12 +12,3 @@ export interface Image {
     createdAt: Date;
     updatedAt: Date;
   }
-
-  // export const sessionOptions: SessionOptions = {
-  //   password: process.env.SESSION_SECRET!,
-  //   cookieName: "user_session",
-  //   cookieOptions: {
-  //     httpOnly: true,
-  //     secure: process.env.NODE_ENV === "production",
-  //   },
-  // }
